4b3b72f205ada12d13a932088b771506b0731c96
Ale <aletiscornia96@gmail.com>
2025-07-07 21:51:45 -0300

    fixed middlewares

--- api/utils/authMiddleware.js
+++ api/utils/authMiddleware.js
@@ -1,22 +1,22 @@
 import jwt from "jsonwebtoken";
-import {errorHandler} from './error.js';
+import { errorHandler } from './error.js';
 
 export const verifyToken = (req, res, next) => {
-    const token = req.cookies.access_token;
-    if(!token){
-        return next(errorHandler(401, 'Sin Autorizacion'));
+  const token = req.cookies.access_token;
+  if (!token) {
+    return next(errorHandler(401, 'Sin Autorización'));
+  }
+
+  jwt.verify(token, process.env.JWT_SECRET, (err, user) => {
+    if (err) {
+      return next(errorHandler(403, 'Token inválido'));
     }
-    jwt.verify(token, process.env.JWT_SECRET, (err, user)=>{
-        if(err){
-            return next(errorHandler(401, 'Token Invalido'));
-        }
-        
-        req.user = user;
-        next();
-    })
+
+    req.user = user;
+    next();
+  });
 };
 
-// Verifica si el usuario es el mismo que está logueado o si es admin
 export const verifyUser = (req, res, next) => {
   verifyToken(req, res, () => {
     if (req.user.id === req.params.id || req.user.isAdmin) {
@@ -27,13 +27,12 @@ export const verifyUser = (req, res, next) => {
   });
 };
 
-// Solo admins pueden continuar
 export const verifyAdmin = (req, res, next) => {
   verifyToken(req, res, () => {
-    if (req.user.isAdmin) {
-      next();
-    } else {
+    if (!req.user || !req.user.isAdmin) {
       return next(errorHandler(403, 'Acceso solo para administradores'));
     }
+
+    next();
   });
 };
