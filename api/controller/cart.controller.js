5810476e5f8ea13c4c7d592b26ce66c0162df150
Ale <aletiscornia96@gmail.com>
2025-07-10 08:03:36 -0300

    cart get and post created

--- api/controller/cart.controller.js
+++ api/controller/cart.controller.js
@@ -12,3 +12,40 @@ export const getCart = async (req, res, next) => {
     next(err);
   }
 };
+
+export const addToCart = async (req, res, next) => {
+  const userId = req.user.id;
+  const { productId, quantity } = req.body;
+
+  if (!productId || !quantity) {
+    return next(errorHandler(400, 'Producto y cantidad son requeridos'));
+  }
+
+  try {
+    let cart = await Cart.findOne({ userId });
+
+    if (!cart) {
+      // Si no hay carrito, lo creamos con el producto
+      cart = new Cart({
+        userId,
+        products: [{ productId, quantity }]
+      });
+    } else {
+      // Buscar si el producto ya está en el carrito
+      const existingProduct = cart.products.find(p => p.productId.toString() === productId);
+
+      if (existingProduct) {
+        // Si ya está, sumamos la cantidad
+        existingProduct.quantity += quantity;
+      } else {
+        // Si no está, lo agregamos
+        cart.products.push({ productId, quantity });
+      }
+    }
+
+    const updatedCart = await cart.save();
+    res.status(200).json(updatedCart);
+  } catch (err) {
+    next(err);
+  }
+};
--- api/routes/cart.route.js
+++ api/routes/cart.route.js
@@ -1,8 +1,10 @@
 import express from 'express';
 import { verifyToken } from '../utils/authMiddleware.js';
-import { getCart } from '../controller/cart.controller.js';
+import { getCart, addToCart, } from '../controller/cart.controller.js';
 
 const router = express.Router();
 router.get('/', verifyToken, getCart);
+router.post('/add', verifyToken, addToCart);
+
 
 export default router;  
