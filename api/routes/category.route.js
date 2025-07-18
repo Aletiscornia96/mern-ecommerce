3f528fcb1e65c928be99bb2bd07bc2f07cec5bce
Ale <aletiscornia96@gmail.com>
2025-07-18 08:44:24 -0300

    post route

--- api/routes/category.route.js
+++ api/routes/category.route.js
@@ -1,7 +1,10 @@
 import express from 'express';
 import { verifyAdmin } from '../Middleware/authMiddleware.js';
+import { createCategory } from '../controller/category.controller.js';
 
 const router = express.Router();
 
 // Crear una nueva categoría (solo admin)
-router.post('/', verifyAdmin );
+router.post('/', verifyAdmin, createCategory );
+
+export default router;
