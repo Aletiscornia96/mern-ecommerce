dbdb23d40c9051d6d75bce6a3fb3fdd6e4201a9f
Ale <aletiscornia96@gmail.com>
2025-07-21 20:05:33 -0300

    category routes added and fixed products model

--- api/routes/category.route.js
+++ api/routes/category.route.js
@@ -1,10 +1,22 @@
 import express from 'express';
 import { verifyAdmin } from '../Middleware/authMiddleware.js';
-import { createCategory } from '../controller/category.controller.js';
+import {
+    createCategory,
+    getAllCategories,
+    getCategoryBySlug,
+    updateCategory,
+    deleteCategory
+} from '../controller/category.controller.js';
 
 const router = express.Router();
 
-// Crear una nueva categoría (solo admin)
-router.post('/', verifyAdmin, createCategory );
+router.post('/', verifyAdmin, createCategory);
+router.get('/', getAllCategories);
+router.get('/:slug', getCategoryBySlug);
+router.patch('/:slug', verifyAdmin, updateCategory);
+router.delete('/:slug', verifyAdmin, deleteCategory);
+
+
+
 
 export default router;
